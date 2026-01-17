e3d1813d5fe0449a1228914a1c172928f6b88fc6
Manav Jain <manav@imPULSE.local>
2026-01-18 00:50:27 +0530

    Fix GTM: Use Next.js Script with beforeInteractive strategy
    - Uses recommended approach for critical third-party scripts
    - beforeInteractive ensures GTM loads before page hydration
    - Should improve detection by Google Tag Assistant

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -137,12 +137,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`dark ${merivaFont.variable} ${sourceSans3Font.variable} ${openSansFont.variable}`}>
       <head>
-        {/* Google Tag Manager - Must be as high in head as possible */}
-        {/* data-cfasync="false" prevents Cloudflare Rocket Loader from deferring this script */}
-        <script
-          data-cfasync="false"
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5F4WVP8J');"
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5F4WVP8J');
+            `,
           }}
         />
         
